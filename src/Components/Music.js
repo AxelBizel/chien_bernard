5210bc71689c0ba3886b70aa47a04989910bd489
Axel Bizel <axel.bizel@gmail.com>
2022-05-06 15:48:46 +0200

    wip

--- src/Components/Music.js
+++ src/Components/Music.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 
 const Music = () => {
   const [isMobile, setIsMobile] = useState(null);
@@ -13,21 +13,32 @@ const Music = () => {
 
   return (
     <Container>
-      <div data-aos="zoom-in">
-        <h2 className="section-titles">Music</h2>
-      </div>
-      <div>
-        <iframe
-          title="Bandcamp"
-          style={{ border: 0, width: "560px", height: "560px" }}
-          src="https://bandcamp.com/EmbeddedPlayer/album=2078108374/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/"
-          seamless
-        >
-          <a href="https://chienbernard.bandcamp.com/album/felindra">
-            Felindra by Chien Bernard
-          </a>
-        </iframe>
-      </div>
+      <Row>
+        <div data-aos="zoom-in">
+          <h2 className="section-titles">Music</h2>
+        </div>
+      </Row>
+      <Row>
+        <Col xs="12" lg="6">
+          <div>
+            <iframe
+              title="Bandcamp"
+              style={{ border: 0, width: "475px", height: "475px" }}
+              src="https://bandcamp.com/EmbeddedPlayer/album=2078108374/size=large/bgcol=333333/linkcol=0f91ff/artwork=small/transparent=true/"
+              seamless
+            >
+              <a href="https://chienbernard.bandcamp.com/album/felindra">
+                Felindra by Chien Bernard
+              </a>
+            </iframe>
+          </div>
+        </Col>
+        <Col xs="12" lg="6">
+          <div>
+            <p>Bla</p>
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 };
