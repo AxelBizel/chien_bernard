300148d16fd2ae916b4cc9ec962e4b89960b6eb3
Axel Bizel <axel.bizel@gmail.com>
2022-05-24 21:59:45 +0200

    imports updates

--- docs/precache-manifest.b7532c2d908a4c8b067a654601e69284.js
+++ docs/precache-manifest.b7532c2d908a4c8b067a654601e69284.js
@@ -1,62 +1,62 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "749b4415a15a9746cc59c63000ef4453",
-    "url": "/chien_bernard/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "ef0e75147ade615f8877",
-    "url": "/chien_bernard/static/css/2.3661fc73.chunk.css"
+    "url": "/static/css/2.3661fc73.chunk.css"
   },
   {
     "revision": "d7af4cd2a858613f99c8",
-    "url": "/chien_bernard/static/css/main.b1e39c32.chunk.css"
+    "url": "/static/css/main.b1e39c32.chunk.css"
   },
   {
     "revision": "ef0e75147ade615f8877",
-    "url": "/chien_bernard/static/js/2.874c5269.chunk.js"
+    "url": "/static/js/2.874c5269.chunk.js"
   },
   {
     "revision": "d7af4cd2a858613f99c8",
-    "url": "/chien_bernard/static/js/main.638c5ea3.chunk.js"
+    "url": "/static/js/main.638c5ea3.chunk.js"
   },
   {
     "revision": "3c7a9545aa8483c1201f",
-    "url": "/chien_bernard/static/js/runtime-main.02fde5be.js"
+    "url": "/static/js/runtime-main.02fde5be.js"
   },
   {
     "revision": "25eecf29c663869e672c8630c70f214d",
-    "url": "/chien_bernard/static/media/absoluteworstshow.25eecf29.jpg"
+    "url": "/static/media/absoluteworstshow.25eecf29.jpg"
   },
   {
     "revision": "f50a9305127e169337f249d16721ea67",
-    "url": "/chien_bernard/static/media/band.f50a9305.jpg"
+    "url": "/static/media/band.f50a9305.jpg"
   },
   {
     "revision": "75464ee5fa4c221d12fb2c68685b4b8a",
-    "url": "/chien_bernard/static/media/bg.75464ee5.jpg"
+    "url": "/static/media/bg.75464ee5.jpg"
   },
   {
     "revision": "4b8e247df3fccac368130f945d86f8d8",
-    "url": "/chien_bernard/static/media/broke_the_casbah.4b8e247d.jpg"
+    "url": "/static/media/broke_the_casbah.4b8e247d.jpg"
   },
   {
     "revision": "2a60dc83e9b709f4a0ae5c0d2da398cb",
-    "url": "/chien_bernard/static/media/header.2a60dc83.gif"
+    "url": "/static/media/header.2a60dc83.gif"
   },
   {
     "revision": "cd1f18f1d58a98d10b1cba687bacf443",
-    "url": "/chien_bernard/static/media/kfuel.cd1f18f1.jpg"
+    "url": "/static/media/kfuel.cd1f18f1.jpg"
   },
   {
     "revision": "86b6ab40529d4f4bfc9bb400dc031b4a",
-    "url": "/chien_bernard/static/media/koolstrings.86b6ab40.jpg"
+    "url": "/static/media/koolstrings.86b6ab40.jpg"
   },
   {
     "revision": "2ef61b7a92f9a1e811eeb7ed27035f53",
-    "url": "/chien_bernard/static/media/noiserus.2ef61b7a.jpeg"
+    "url": "/static/media/noiserus.2ef61b7a.jpeg"
   },
   {
     "revision": "7dd37ce7eb634aecea5228c1626f5c3a",
-    "url": "/chien_bernard/static/media/prl.7dd37ce7.jpg"
+    "url": "/static/media/prl.7dd37ce7.jpg"
   }
 ]);
